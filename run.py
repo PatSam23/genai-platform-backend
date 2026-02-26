c8d9a8a5a570fa112c49346eee782aa7758eedc8
Samarth Pathak <pathaksamarth11@gmail.com>
2026-02-26 23:08:31 +0530

    Fix indentation and clean up run.py

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os, sys, subprocess
 path = "Scripts" if sys.platform == "win32" else "bin"
 cmd1 = [os.path.join("venv", path, "activate")]
 cmd2 = [os.path.join("venv", path, "python"), "-m", "uvicorn", "app.main:app", "--reload"]
-
+ 
 subprocess.run(cmd1, shell=True)  
 subprocess.run(cmd2)
